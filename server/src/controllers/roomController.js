8ab43216425cefa77c7ecd3f397ae04e9c32f3e5
Phan Kien <phankien110302@gmail.com>
2023-08-24 02:24:40 +0700

    create api create room and add member to room

---
+++ server/src/controllers/roomController.js
@@ -0,0 +1,66 @@
+import Room from "../models/Room";
+import participants from "../models/Participants";
+import user from "../models/User";
+
+// * tạo phòng chat
+export const createRoom = async (req,res) =>{
+    const newroom = req.body;
+    const leader = await user.findById(newroom.leader)
+    // * kiểm tra xem leader có tồn tại không
+    if(leader){
+        // * nếu có tạo phòng chat mới
+        Room.create({
+            nameRoom: newroom.nameRoom,
+            leader :    newroom.leader ,
+            active : true,
+        }).then(response =>{
+            // * nếu tạo phòng thành công thêm danh sách thành viên trong phòng
+            participants.create({
+                idUser : newroom.leader,
+                idRoom : response.id
+            }).then(response =>{
+                console.log(response);
+                return res.status(200).json({message : "create room success", data : response})
+            })
+            .catch(error =>{
+                console.log(error);
+                return res.status(400).json({message : "create rooms faild", data : {}})
+            })
+        }).catch(error =>{
+            console.log(error);
+            return res.status(400).json({message : "create rooms faild", data : {}})
+        })
+    }
+    else{
+        return res.status(400).json({message : "user not found", data : {}})
+    }
+}
+// * thêm thành viên vào phòng chat
+export const addMembertoRoom = async (req,res)=>{
+    const newMember = req.body;
+    // * kiểm tra user có tồn tại không
+    const checkUser = await  user.findOne({_id: newMember.idUser, active : true});
+    if(!checkUser)
+        return res.status(400).json({message : "user not found", data : {}})
+    // * kiểm tra phòng có tồn tại không
+    const checkRoom = await Room.findOne({_id: newMember.idRoom, active : true})
+    if(!checkRoom)
+        return res.status(400).json({message : "Can't add members to this group", data : {}})
+    // * kiểm tra user đã tham gia phòng chat chưa
+    const checkUserInRoom = await participants.findOne({idRoom : newMember.idRoom});
+    console.log(checkUserInRoom.idUser);
+    if(checkUserInRoom.idUser.includes(checkUser._id))
+        return res.status(400).json({message : "This user has joined the room", data : {}})
+    // * thêm user vào phòng 
+    participants.updateOne({idRoom : newMember.idRoom},{
+        $push:{
+            idUser : checkUser._id
+        }
+    })
+    .then(response =>{
+        return res.status(200).json({message : "add member success", data : response})
+    }).catch(error =>{
+        console.log(error);
+        return res.status(200).json({message : "add member faild", data : {}})
+    })
+}
--- server/src/routers/indexRouter.js
+++ server/src/routers/indexRouter.js
@@ -8,6 +8,9 @@ const Indexrouter = (app)=>{
     Router.post("/create-user",userController.createUser)
     Router.post("/active-user",userController.activeUser)
     Router.post("/login",userController.login)
+
+    Router.post("/create-room",require("../controllers/roomController").createRoom)
+    Router.post("/add-member",require("../controllers/roomController").addMembertoRoom)
     return app.use("/api",Router);
 }
 
