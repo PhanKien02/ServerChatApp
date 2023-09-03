702ddb1d97ec2dffac2762295a769d252dd60a96
Phan Kien <phankien110302@gmail.com>
2023-09-04 01:52:49 +0700

    config model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
         firstName: String,
         lastName : String,
-        userName : String,
         password :  String,
         email :{type : String , unique : true,require : [true, 'User email number required']},
         phone : String,
